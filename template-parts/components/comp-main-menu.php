05a23187b555e6d064883837501cdf8b0bf7b188
“Hadhops” <martin.j.hadley@gmail.com>
2026-09-02 13:58:28 +1000

    flipped manu nav layout

--- template-parts/components/comp-main-menu.php
+++ template-parts/components/comp-main-menu.php
@@ -20,21 +20,6 @@ $box_links = array_slice($box_links, 0, 4);
         </a>
       </div>
       
-
-      <div class="col-xl-6 col-8">
-        <ul class="main-menu__box-links">
-          <?php foreach ($box_links as $box): $id = $box->ID; ?>
-          <li>
-            <a href="<?= $box->url; ?>" class="box-links__content">
-                <?= wp_get_attachment_image(get_field('image', $id), 'full'); ?>
-                <h3><?= $box->title; ?></h3>
-                <p><?php the_field('description', $id); ?></p>
-            </a>
-          </li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-
       <div class="col-xl-3 col-4 d-flex flex-column justify-content-between">
         <?php
                 wp_nav_menu( array(
@@ -51,27 +36,21 @@ $box_links = array_slice($box_links, 0, 4);
         </div>
       </div>
 
-      
+      <div class="col-xl-6 col-8">
+        <ul class="main-menu__box-links">
+          <?php foreach ($box_links as $box): $id = $box->ID; ?>
+          <li>
+            <a href="<?= $box->url; ?>" class="box-links__content">
+                <?= wp_get_attachment_image(get_field('image', $id), 'full'); ?>
+                <h3><?= $box->title; ?></h3>
+                <p><?php the_field('description', $id); ?></p>
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>      
 
     </div>
   </div>
 </nav>
 
-<!-- <nav class="main-menu">
-    <div class="container position-relative d-flex justify-content-center align-items-center h-100">
-        <button data-toggle-main-menu class="main-menu__close"></button>
-        <div class="main-menu__inner">
-            <img class="main-menu__logo"
-                src="<?= get_stylesheet_directory_uri(); ?>/static/logos/Canyon_Logo_Black.svg"
-                alt="Canyon Logo">
-            <?php
-                wp_nav_menu( array(
-                    'theme_location'    => 'menu-1',
-                    'depth'             => 2,
-                    'container'         => '',
-                    'menu_id' 			=> 'main-menu'
-                ) );
-                ?>
-        </div>
-    </div>
-</nav> -->
